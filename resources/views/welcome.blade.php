5216ed03e73b1e295387b249a55e36dddbb7ded1
Alma Z <almat.izm@gmail.com>
2017-10-12 20:01:46 +0600

    Выравнивание отступов в верстке

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,15 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>
-Home page
-	</title>
+    <meta charset="UTF-8">
+    <title>
+        Home page
+    </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Latest compiled and minified CSS -->
     {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">--}}
 
-    <link rel="stylesheet" href="/css/bootstrap.css" >
+    <link rel="stylesheet" href="/css/bootstrap.css">
     <script src="/js/jquery-3.1.1.js"></script>
 </head>
 <body>
@@ -23,52 +23,52 @@ Home page
                 <span>День:</span>
                 <p id="day">0</p>
 
-            <?$index = 1?>
-            @foreach($paddock as $key => $list)
-                <div class="col-md-6">
-                    <h3>Загон №{{ $key  }}</h3>
+                <?$index = 1?>
+                @foreach ($paddock as $key => $list)
+                    <div class="col-md-6">
+                        <h3>Загон №{{ $key  }}</h3>
 
-                    <div id="paddock{{ $key  }}" class="zagon">
-                        {{--@for($i = 1; $i <= $value; $i++, $index++ )--}}
-                        @foreach($list as $sheepId)
-                            <div id="sheep{{ $sheepId }}" class="name">Sheep{{ $sheepId }}</div>
-                        {{--@endfor--}}
-                        @endforeach
+                        <div id="paddock{{ $key  }}" class="zagon">
+                            {{--@for($i = 1; $i <= $value; $i++, $index++ )--}}
+                            @foreach ($list as $sheepId)
+                                <div id="sheep{{ $sheepId }}" class="name">Sheep{{ $sheepId }}</div>
+                                {{--@endfor--}}
+                            @endforeach
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
             </fieldset>
 
 
-
-                <div class="col-md-6">
-                    <form>
-                        <div class="form-group">
-                            <button id="reset" class="btn btn-danger">Reset</button>
-                            {{--<button id="sleep" class="btn btn-warning">Sleep</button>--}}
-                        </div>
-                        <div class="form-group">
-                            {{--<input type="text" placeholder="Enter command">--}}
-                            <select name="command" id="">
-                                <option value="add">Add</option>
-                                <option value="sleep">Sleep</option>
-                            </select>
-                            <input type="submit" name="send" value="Send Command">
-                        </div>
-                    </form>
-                </div>
+            <div class="col-md-6">
+                <form>
+                    <div class="form-group">
+                        <button id="reset" class="btn btn-danger">Reset</button>
+                        {{--<button id="sleep" class="btn btn-warning">Sleep</button>--}}
+                    </div>
+                    <div class="form-group">
+                        {{--<input type="text" placeholder="Enter command">--}}
+                        <select name="command" id="">
+                            <option value="add">Add</option>
+                            <option value="sleep">Sleep</option>
+                        </select>
+                        <input type="submit" name="send" value="Send Command">
+                    </div>
+                </form>
+            </div>
 
             <div class="col-md-7 info-block">
                 <p><a href="/stat/">Общая статистика</a></p>
                 <p class="bg-danger">Reset = Очищает таблицу овец и историю</p>
                 <p class="bg-success">Add = Добавляет одну овцу</p>
-                <p class="bg-info">Sleep = Убирает одну овечку. Если в каком то загоне осталось одна овечка, переводит из самой насленной</p>
+                <p class="bg-info">Sleep = Убирает одну овечку. Если в каком то загоне осталось одна овечка, переводит
+                    из самой насленной</p>
             </div>
-            </div>
+        </div>
 
 
-            {{--</form>--}}
-</div>
+        {{--</form>--}}
+    </div>
 </div>
 
 <style>
